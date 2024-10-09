b0447c61ba1e063f1b9020445cca7cd1a4cd9a74
Usual <a2628558055@163.com>
2024-10-09 17:40:23 +0800

    fix: 404 2

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (req, res) => {
   let target = '';
+  let target1 = '';
+  let target2 = '';
   // 根据请求路径设置不同的代理目标
   if (req.url.startsWith('/api')) {
-    target = 'https://v1.hitokoto.cn'; 
+    target = 'https://v1.hitokoto.cn';
   } else if (req.url.startsWith('/kuleu')) {
-    target = 'https://api.kuleu.com/api/'; 
+    target1 = 'https://api.kuleu.com/api/';
   } else if (req.url.startsWith('/pexelsPicAndVideo')) {
-    target = 'https://api.pexels.com'; 
+    target2 = 'https://api.pexels.com';
   }
 
   // 如果target为空，说明没有匹配到任何代理规则，可以直接返回或处理错误
@@ -24,7 +26,33 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 根据不同的路径前缀重写路径
       '^/api/': '/',
+    },
+    // 可选：添加日志或其他中间件选项
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(`[Proxy] ${req.method} ${req.url}`);
+    },
+  })(req, res);
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target: target1,
+    changeOrigin: true,
+    pathRewrite: {
+      // 根据不同的路径前缀重写路径
+
       '^/kuleu/': '/',
+    },
+    // 可选：添加日志或其他中间件选项
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(`[Proxy] ${req.method} ${req.url}`);
+    },
+  })(req, res);
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target: target2,
+    changeOrigin: true,
+    pathRewrite: {
+      // 根据不同的路径前缀重写路径
+
       '^/pexelsPicAndVideo/': '/',
     },
     // 可选：添加日志或其他中间件选项
